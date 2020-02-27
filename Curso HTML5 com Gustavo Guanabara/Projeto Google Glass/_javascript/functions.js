eb5397142fa646e9150f050bc79c645432abfcce
Daniel Mendes do Amaral <61394952+DanielMendesdoAmaral@users.noreply.github.com>
2020-02-27 00:04:11 -0300

    Projeto Google Glass

---
+++ Curso HTML5 com Gustavo Guanabara/Projeto Google Glass/_javascript/functions.js
@@ -0,0 +1,3 @@
+function mudaFoto(foto) { 
+    document.getElementById("icone").src = foto; //Quando tiver onmouseover, ele indicará o caminho da foto e armazenará na variável src, e trocará a foto.
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-googleglass-html5
+Projeto criado no curso de HTML5, CSS3 e JavaScript, ofertado pelo Curso em Vídeo. Foram as primeiras linguagens que eu aprendi.
